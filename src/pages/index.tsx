5a1010836bc7ce430a379911294072a259158e79
salluthdev <salluthdev@gmail.com>
2023-02-17 13:50:17 +0700

    feat: install and config tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,7 +12,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main>
+        <div className="w-40 h-40 bg-stone-800">
+          <h1 className="text-[aqua]">Halo!</h1>
+        </div>
+      </main>
     </>
   );
 }
