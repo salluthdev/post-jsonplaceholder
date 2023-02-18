2e46aa7779f6bd2f7b524172cd6f0a8a95d205ad
salluthdev <salluthdev@gmail.com>
2023-02-18 21:14:28 +0700

    fix: responsive card in mobile

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,11 +1,25 @@
-interface ButtonProps {
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  variant?: "primary" | "outline";
 }
 
-export default function Button({ children, ...props }: ButtonProps) {
+const variants = {
+  primary: "text-stone-800 bg-[aqua] border border-stone-800",
+  outline: "text-[aqua] bg-stone-800 border border-[aqua]",
+};
+
+export default function Button({
+  children,
+  variant = "primary",
+  ...props
+}: ButtonProps) {
+  let variantClasses = variants[variant];
+
   return (
     <button
-      className="text-sm font-semibold text-stone-800 bg-[aqua] border border-stone-800 rounded-sm py-2 px-4"
+      className={`text-sm font-semibold rounded-sm py-2 px-4 ${variantClasses}`}
       {...props}
     >
       {children}
--- src/components/pages/CardWrapper.tsx
+++ src/components/pages/CardWrapper.tsx
@@ -1,28 +1,65 @@
 import { useEffect, useState } from "react";
-import { Card } from "../common";
+import { Button, Card } from "../common";
 
 export default function CardWrapper() {
+  const [postData, setPostData] = useState([]);
+  const [idStart, setIdStart] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(
+      `https://jsonplaceholder.typicode.com/posts?_start=${idStart}&_limit=10`
+    )
       .then((res) => res.json())
       .then((data) => setPostData(data));
-  }, []);
-
-  const [postData, setPostData] = useState([]);
+  }, [idStart]);
 
   return (
     <div className="wrapper">
-      <div className="flex flex-wrap justify-between gap-4 py-10">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-between gap-4 py-10">
         {postData.map((item: any) => {
           return (
             <div
               key={item.id}
-              className="min-w-[360px] flex-1 border-[2px] border-[aqua] rounded-sm"
+              className="min-w-[100%] sm:min-w-[360px] flex-1 border-[2px] border-[aqua] rounded-sm"
             >
               <Card title={item.title} content={item.body} id={item.id} />
             </div>
           );
         })}
+      </div>
+      <div className="mb-10">
+        <div className="flex flex-wrap items-end gap-4">
+          {currentPage > 1 && (
+            <Button
+              variant="outline"
+              onClick={() => {
+                setIdStart(idStart - 10);
+                setCurrentPage(currentPage - 1);
+              }}
+            >
+              Prev
+            </Button>
+          )}
+          <div className="flex items-end gap-2">
+            {currentPage > 1 && (
+              <Button variant="outline">{currentPage - 1}</Button>
+            )}
+            <Button>{currentPage}</Button>
+            <Button variant="outline">{currentPage + 1}</Button>
+          </div>
+          {postData !== null && (
+            <Button
+              variant="outline"
+              onClick={() => {
+                setIdStart(idStart + 10);
+                setCurrentPage(currentPage + 1);
+              }}
+            >
+              Next
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
