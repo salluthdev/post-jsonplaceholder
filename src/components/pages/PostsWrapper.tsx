f147ff657b5efe90ec26bf2f19ccbcb79af87630
salluthdev <salluthdev@gmail.com>
2023-02-19 14:40:53 +0700

    refactor: delete useless card style

--- src/components/pages/PostsWrapper.tsx
+++ src/components/pages/PostsWrapper.tsx
@@ -10,10 +10,7 @@ export default function PostsWrapper({ postData }: PostsWrapperProps) {
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-between gap-4 py-10">
       {postData.map((item: Post) => {
         return (
-          <div
-            key={item.id}
-            className="min-w-[100%] sm:min-w-[360px] flex-1 border-[2px] border-[aqua] rounded-sm"
-          >
+          <div key={item.id} className="border-[2px] border-[aqua] rounded-sm">
             <Card title={item.title} content={item.body} id={item.id} />
           </div>
         );
